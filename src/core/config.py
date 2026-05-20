2d3b56309e1a78d452b58d0f4801953419c53e06
Vladyslav <icaevvlad81@gmail.com>
2026-05-20 17:18:23 +0300

    create base project structure

---
+++ src/core/config.py
@@ -0,0 +1,51 @@
+from decouple import config, Csv
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pydantic.typing import SecretStr
+
+class JWTConfig(BaseModel):
+    ACCESS_SECRET_KEY: SecretStr = config('ACCESS_SECRET_KEY', default='secret')
+    REFRESH_SECRET_KEY: SecretStr = config('REFRESH_SECRET_KEY', default='secret_refresh')
+    _ACCESS_TIME_TO_EXPIRE: int = 30  # in minutes
+    _REFRESH_TIME_TO_EXPIRE: int = 7  # in days
+    _ALGORITHM: str = 'HS256'
+
+    @property
+    def access_time_to_expire(self):
+        return self._ACCESS_TIME_TO_EXPIRE
+
+    @access_time_to_expire.setter
+    def access_time_to_expire(self, value: int):
+        """TAKE TIME IN MINUTES"""
+        if value:
+            self._ACCESS_TIME_TO_EXPIRE = value
+
+    @property
+    def refresh_time_to_expire(self):
+        return self._REFRESH_TIME_TO_EXPIRE
+
+    @refresh_time_to_expire.setter
+    def refresh_time_to_expire(self, value: int):
+        """TAKE TIME IN DAYS"""
+        if value:
+            self._REFRESH_TIME_TO_EXPIRE = value
+
+    @property
+    def algorithm(self):
+        """Crypto algorythm for coding jwt data \n
+            default: HS256"""
+        return self._ALGORITHM
+
+    @algorithm.setter
+    def algorithm(self, value: str):
+        if value:
+            self._ALGORITHM = value
+
+class Settings(BaseSettings):
+    host: str = config("HOST", cast=Csv, default="localhost,127.0.0.1")
+    port: int = config("PORT", cast=int, default=8000)
+    DATABASE_URL: str = config('DATABASE_URL', cast=str, default="sqlite:///books.db")
+    token: JWTConfig = JWTConfig()
+
+
+settings = Settings()
